873731b2d1994231e6c59dc6f31fb6e0d74954ee
jithinsebastian <jithinsebastian@qburst.com>
2017-01-24 16:55:31 +0530

    Added solution

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # moviesort
+
+Create a folder vet in home and copy all media there, run the script. It automatically rename files based on genre and place it in according folder. Currently Movies and Series supported.
+
+Uses <a href="https://github.com/guessit-io/guessit">guessit</a>.
---
+++ sort.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+from guessit import guessit
+
+MOVIEDB = '/home/user/Videos'
+
+VET = '/home/user/Vet'
+
+
+def find(folder):
+    for newMovie in os.listdir(folder):
+        movieInfo = guessit(newMovie)
+        try:
+            if 'container' in json.dumps(movieInfo):
+                shutil.move(folder + '/' + newMovie, createDestinationDirectory(folder + '/' + movieInfo['title']))
+                newMovie = movieInfo['title']
+        except:
+
+        if movieInfo['type'] == 'movie':
+            moveDataAndDeleteFolder(folder + '/' + newMovie, createDestinationDirectory(MOVIEDB + '/Movies/' + movieInfo['title']))
+        elif movieInfo['type'] == 'episode':
+            moveDataAndDeleteFolder(folder + '/' + newMovie, createDestinationDirectory(MOVIEDB + '/Series/' + movieInfo['title'] + '/' + movieInfo['episode_title']))
+
+
+def createDestinationDirectory(destinationDir):
+    if not os.path.exists(destinationDir):
+        os.makedirs(destinationDir)
+    return destinationDir
+
+
+def moveDataAndDeleteFolder(moviefolder, destinationDir):
+    for fl in os.listdir(moviefolder):
+        path = moviefolder + '/' + fl
+        shutil.move(path, destinationDir)
+    shutil.rmtree(moviefolder)
+
+find(VET)
